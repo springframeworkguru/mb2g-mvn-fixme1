a040f539cc380760041c70b1923fcfbf7943206f
Ronnen <nagalr@gmail.com>
2021-05-12 16:37:51 +0300

    Modified to a working Project.

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,13 +1,15 @@
 package guru.springframework;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaHelloWorldTest {
 
     @Test
     public void getHello() {
+        System.out.println("My JUnit-1 Run");
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
 
         assertEquals("Hello World", javaHelloWorld.getHello());
